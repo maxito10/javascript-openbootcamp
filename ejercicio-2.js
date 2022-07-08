25021d548b145af7416e4ae46369067394aed3bc
maxito10 <maxi.arguello10@hotmail.com>
2022-07-08 16:59:43 -0300

    Add files via upload

---
+++ ejercicio-2.js
@@ -0,0 +1,10 @@
+const nombre = "Jorge Maximiliano Argüello";
+const edad = 34;
+const desarrolador = true;
+const fechaDeNacimiento = new Date("may 25 1988" );
+const libroFavorito = {
+    nombre: "El hombre mas rico de babilonia",
+    autor: "George S. Clason",
+    fecha: 1926,
+}
+
