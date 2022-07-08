b44d64c3594d9d2817d02ccd5b5eb48ba2698a48
maxito10 <maxi.arguello10@hotmail.com>
2022-07-08 17:06:39 -0300

    Add files via upload

--- ejercicio-2.js
+++ ejercicio-2.js
@@ -5,6 +5,7 @@ const fechaDeNacimiento = new Date("may 25 1988" );
 const libroFavorito = {
     nombre: "El hombre mas rico de babilonia",
     autor: "George S. Clason",
-    fecha: 1926,
+    fecha: new Date(1926, 0 , 1) ,
+    URL : "https://www.amazon.com/-/es/George-S-Clason/dp/8477203717",
 }
 
