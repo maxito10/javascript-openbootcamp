3f585c942e6294b2db6177ae0a0d60f3aa20dfae
Jorge Maximiliano Arguello <maxi.arguello10@hotmail.com>
2022-08-15 16:11:45 -0300

    Add files via upload

---
+++ Fecha.js
@@ -0,0 +1,5 @@
+const fechaHoy = new Date();
+const fechaNacimiento = new Date("mayo, 25,1988");
+const hoyEsMas = fechaHoy.getTime()> fechaNacimiento.getTime();
+const miMes = new Date(4);
+const miAnio = new Date(1988);
