a2f6f2ddc10dd96ce21d301a87a0e97bddb88a94
Jorge Maximiliano Arguello <maxi.arguello10@hotmail.com>
2022-08-15 16:18:02 -0300

    Update Fecha.js

--- Fecha.js
+++ Fecha.js
@@ -1,5 +1,6 @@
 const fechaHoy = new Date();
-const fechaNacimiento = new Date("mayo, 25,1988");
+const fechaNacimiento = new Date(1988,04,25);
 const hoyEsMas = fechaHoy.getTime()> fechaNacimiento.getTime();
-const miMes = new Date(4);
-const miAnio = new Date(1988);
+const diaNacimiento = fechaNacimiento.getDate()
+const miMes = fechaNacimiento.getMonth() +1;
+const miAnio = fechaNacimiento.getFullYear();
